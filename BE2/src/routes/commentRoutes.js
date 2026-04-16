fa1fe65402d13012589ca2493006b020d57b0a78
Nguyen Thang <dh52201437@student.stu.edu.vn>
2026-04-16 16:50:59 +0700

    install swagger

--- BE2/src/routes/commentRoutes.js
+++ BE2/src/routes/commentRoutes.js
@@ -1,15 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const { getAllComments, deleteComment } = require('../controllers/commentController');
 
-const {
-  getAllComments,
-  deleteComment
-} = require('../controllers/commentController');
 
-// Đường dẫn: GET /comments
 router.get('/', getAllComments);
-
-// Đường dẫn: DELETE /comments/:id (Nhớ truyền _id của MongoDB vào đây)
 router.delete('/:id', deleteComment);
 
 module.exports = router;
