11a133452da617168395957e96b0d3fe6685efaa
Nguyen Thang <dh52201437@student.stu.edu.vn>
2026-04-16 16:50:59 +0700

    install swagger

--- BE2/src/routes/adminRoutes.js
+++ BE2/src/routes/adminRoutes.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const router = express.Router();
-
 const { getDashboardStats } = require('../controllers/adminController');
 const { isAdmin } = require('../middlewares/authMiddleware');
 
-// Đường dẫn: GET /admin/stats
-// Phải có isAdmin để chặn các User 
 router.get('/stats', isAdmin, getDashboardStats);
 
 module.exports = router;
--- BE2/src/routes/userRoutes.js
+++ BE2/src/routes/userRoutes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-// 1. Import các hàm đã viết bên userController
 const {
     getAllUsers,
     getUserById,
@@ -10,20 +9,11 @@ const {
     deleteUser
 } = require('../controllers/userController');
 
-// 2. Gắn các hàm đó vào đúng đường dẫn (Endpoints)
-// Đường dẫn: BASE_API/users
+
 router.get('/', getAllUsers);
-
-// Đường dẫn: BASE_API/users/:id 
-router.get('/:id', getUserById);
-
-// Đường dẫn: BASE_API/users (Dùng POST để thêm mới)
 router.post('/', createUser);
-
-// Đường dẫn: BASE_API/users/:id (Dùng PUT để sửa)
+router.get('/:id', getUserById);
 router.put('/:id', updateUser);
-
-// Đường dẫn: BASE_API/users/:id (Dùng DELETE để xóa)
 router.delete('/:id', deleteUser);
 
 module.exports = router;
